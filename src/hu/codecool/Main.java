38767388f014def0ae6c259bb74243f804b4ef02
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-08-12 13:20:06 +0200

    idea

--- src/hu/codecool/Main.java
+++ src/hu/codecool/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    createVehicles();
-	    simulateRace();
-	    printRaceResults();
+	    // simulateRace();
+	    // printRaceResults();
     }
 
     private static boolean isRaining() {
