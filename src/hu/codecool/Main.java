06f4d7c1a6a29a96a51ec1e27edea6ecf19bcf3e
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-08-14 16:13:17 +0200

    implement motorcycles

--- src/hu/codecool/Main.java
+++ src/hu/codecool/Main.java
@@ -5,6 +5,8 @@ import sun.awt.datatransfer.DataTransferer;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static hu.codecool.Motorcycle.createName;
+
 public class Main {
 
     static Random random = new Random();
@@ -13,14 +15,16 @@ public class Main {
     public static void main(String[] args) {
         
         Car.setSpeedLimit(110);
-        Car[] vehicles = createVehicles();
+        // Car[] vehicles = createVehicles();
+        Motorcycle[] vehicles = createVehicles();
         vehicles = simulateRace(vehicles);
         printRaceResults(vehicles);
 
     }
 
-    private static Car[] createVehicles() {
+    private static Motorcycle[] createVehicles() {
 
+        /*
         Car[] cars = new Car[10];
         String[] carNames = Car.createNames();
         for (int i = 0; i < 10; i++) {
@@ -29,8 +33,15 @@ public class Main {
             car.setNormalSpeed();
             cars[i] = car;
         }
-
         return cars;
+        */
+        Motorcycle[] motos = new Motorcycle[10];
+        for (int i = 0; i < 10; i++) {
+            Motorcycle moto = new Motorcycle();
+            moto.name = createName();
+            motos[i] = moto;
+        }
+        return motos;
     }
 
     private static boolean isRaining() {
@@ -41,64 +52,41 @@ public class Main {
             return false;
     }
 
-    private static Car[] simulateRace(Car[] vehicles) {
+    private static Motorcycle[] simulateRace(Motorcycle[] vehicles) {
 
-        int[] raceStat = new int[500];
+        // int[] raceStat = new int[500];
 
         for (int hour = 1; hour <= 50; hour++) {
+            /*
             if (isRaining()) {
                 // System.out.println("Raining!");
             } else {
                 // System.out.println("Nice and clear!");
             }
+            */
             for (int i = 0; i < 10; i++) {
-                vehicles[i].moveForAnHour();
-                raceStat[(hour-1)*10+i] = (vehicles[i].distanceTraveled);
+                vehicles[i].moveForAnHour(isRaining());
+                // raceStat[(hour-1)*10+i] = (vehicles[i].distanceTraveled);
             }
         }
-        /*
-        int counter = 0;
-        for (int hour = 1; hour < 50; hour++) {
-            for (int i = 0; i < 10; i++) {
-                counter = 0;
-                for (int s = 0; s < raceStat[(hour-1)*10+i]/100; s++) {
-                    // System.out.print('*');
-                    counter++;
-                }
-                // System.out.println(counter);
-            }
-            // TimeUnit.SECONDS.sleep(1);
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            for (int i = 0; i < counter; i++) {
-                // System.out.print("\b");
-            }
-
-            // System.out.println();
-        }
-        */
         return vehicles;
     }
 
-    private static void printRaceResults(Car[] vehicles) {
+    private static void printRaceResults(Motorcycle[] vehicles) {
 
-        List<Car> cars = new ArrayList<>();
-        for (Car car: vehicles){
-            cars.add(car);
+        List<Motorcycle> motos = new ArrayList<>();
+        for (Motorcycle moto: vehicles){
+            motos.add(moto);
         }
 
         List<String> result = new ArrayList<>();
-        cars.stream()
-                .sorted(Comparator.comparing(Car::getDistanceTraveled).reversed())
-                .forEach(c -> result.add(c.name + " " + c.distanceTraveled));
+        motos.stream()
+                .sorted(Comparator.comparing(Motorcycle::getDistanceTraveled).reversed())
+                .forEach(v -> result.add(v.name + " " + v.distanceTraveled));
 
         int counter = 0;
-        for (String car: result) {
-            System.out.println(++counter + ".: " + car);
+        for (String vehicle: result) {
+            System.out.println(++counter + ".: " + vehicle);
         }
     }
  }
--- src/hu/codecool/Motorcycle.java
+++ src/hu/codecool/Motorcycle.java
@@ -1,4 +1,57 @@
 package hu.codecool;
 
+import java.util.Random;
+
 public class Motorcycle {
+
+    static int defaultSpeed = 100;
+    static int nameNumber = 1;
+    String name;
+    int distanceTraveled;
+
+
+    public void moveForAnHour(boolean isRaining) {
+        Random random = new Random();
+        int speed = isRaining ? defaultSpeed - random.nextInt(46) + 5 : defaultSpeed;
+        distanceTraveled += speed;
+    }
+
+    public static String createName() {
+
+        String name = "Motorcycle " + nameNumber++;
+        return name;
+    }
+
+    public int getDistanceTraveled() {
+        return distanceTraveled;
+    }
 }
+
+/*
+Motorcycle // speed is 100km/h. If rains, travels with 5-50km/h slower (randomly).
+static nameNumber // The number of the instance created. Used for its name.
+        name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
+        distanceTraveled
+        moveForAnHour()
+*/
+
+    /*
+    public static void setSpeedLimit(int raceSpeedLimit) {
+        speedLimit = raceSpeedLimit;
+    }
+     */
+
+    /*
+        public void setNormalSpeed() {
+        Random random = new Random();
+        normalSpeed = random.nextInt(31) + 80;
+    }
+     */
+
+    /*
+    public String getName() {
+        return name;
+    }
+    */
+
+
